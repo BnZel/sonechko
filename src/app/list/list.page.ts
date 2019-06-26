cfbeeff2033640678e3f1146f4709e328f307b80
Jason T <biozxel1111@gmail.com>
2019-06-25 20:20:34 -0400

    jason-Added favorite icons

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,16 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ListPage implements OnInit {
   private selectedItem: any;
   private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
     'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
+    'ios-add',
+    'ios-alarm'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
